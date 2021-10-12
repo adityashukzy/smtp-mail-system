0a7aa5b22bd7ccb09f496ea4fba84b339592de42
Aditya Shukla <adityashukzy@gmail.com>
2021-10-12 10:26:20 +0530

    Beautify terminal interface with Rich and restructure code

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
+from rich import print
 from mail import Mail
 from datetime import datetime
 import server_A, server_B, client
 
 def main():
-    print("\n\n~~~~~~~~~ Welcome to the SMTP Mail Service ~~~~~~~~~\n\n")
+    print("\n\n\t\t[bold magenta]SMTP Mail Service[/bold magenta]\n")
 
-    grp = input("Are you part of group A or group B? (type A for group A, B for group B)")
+    grp = input("Are you part of group A or group B?\n")
+    print(f"Welcome to group {grp}!")
+
+    print("Operations: ")
+    print("1. Send an email")
+    print("2. Access mailbox")
+    option = int(input("Select which operation you would like to do (enter 1 or 2):  "))
     
     if grp == "A":
-        print("Welcome to group A!")
-
-        print("Operations: ")
-        print("1. Send an email")
-        print("2. Access mailbox")
-        option = int(input("Select which operation you would like to do (enter 1 or 2):  "))
-
         if option == 1:
-            print("\nLet's write the email now!\n")
+            print("\n\t[u]Let's write the email now![/u]\n")
             
             # constructing the email with the help of writeEmail function
-            # email = client.writeEmail()
-            email = Mail('123@gmail.com', '456@hotmail.com', datetime.now(), 'Hey!', "BYE\nBoY")
+            email = client.writeEmail()
+            # email = Mail('123@gmail.com', '456@hotmail.com', datetime.now(), 'Hey!', "BYE\nBoY")
 
             # sending the email to the server
             client.sendEmailToServer(email)
